a30b39240a8ca02f1d7498a2de140515455f82e7
Chris Hornsey <chris@thehornseys.com>
2023-06-18 15:49:10 +0100

    set up firebase hosting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKYEzGpp3k8t5J4K3_ZMQNvr51bqUJcJ0",
+  authDomain: "austinrecipestorage.firebaseapp.com",
+  projectId: "austinrecipestorage",
+  storageBucket: "austinrecipestorage.appspot.com",
+  messagingSenderId: "872560981020",
+  appId: "1:872560981020:web:3eb374306664e348005bd9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
