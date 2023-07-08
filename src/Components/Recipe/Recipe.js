7d443c049721f0db2428d9b2d24ac8015045b00f
Chris Hornsey <chris@thehornseys.com>
2023-07-08 08:36:49 +0100

    added background image

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -5,8 +5,11 @@ import { TagAdder } from "../TagAdder/tagAdder";
 
 export default function Recipe(props) {
 
+    const style = {backgroundColor: props.bgColor}
+    
+
     return (
-        <div className = "Recipe">
+        <div className = "Recipe" style={style}>
             <h2><a href={props.recipe.URL}>{props.recipe.Title}</a></h2>
             <TagAdder addTag={props.addTag} recipeID={props.recipe.id} isFirstTag={props.recipe.Tags ? false : true}/>
             {props.recipe.Tags ? <TagsList tags = {props.recipe.Tags} RecipeID={props.recipe.id} removeTag={props.removeTag}/> : ""}
--- src/Components/Recipelist/RecipeList.js
+++ src/Components/Recipelist/RecipeList.js
@@ -1,14 +1,23 @@
 import React from "react";
 import './RecipeList.css'
 import Recipe from "../Recipe/Recipe";
+import background from "../../Images/bakednesca.jpg"
 
 
 export default function RecipeList (props) {
 
+    const colours = ["lightblue", "lightcyan", "lightgoldenrodyellow", "lightgreen", "lightpink", "lightsalmon", "lightsteelblue", "lightyellow", "lightskyblue", "mistyrose", "palegreen", "peachpuff", "lavender", "beige", "lavenderblush"]
+
+    function randomColor() {
+        const randomNumber = Math.floor(Math.random()*colours.length)
+
+        return colours[randomNumber]
+    }
+
     return (
-        <div className = "RecipeList">
+        <div className = "RecipeList" style={{ backgroundImage: `url(${background})`}}>
                 {props.recipes.length > 0 ? (
-                    props.recipes.map(recipe => <Recipe key = {recipe.id} recipe={recipe} addTag={props.addTag} removeTag={props.removeTag}/>)
+                    props.recipes.map(recipe => <Recipe key = {recipe.id} recipe={recipe} addTag={props.addTag} removeTag={props.removeTag} bgColor={randomColor()}/>)
                     ) : (
                         <div>
                         <p>Log in to see recipes</p>
